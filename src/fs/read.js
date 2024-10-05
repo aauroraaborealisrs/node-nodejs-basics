b1fc909ab1af9604765ea402b4dc3e2ce9bc1c85
aauroraaborealisrs <imaginepeach@mail.ru>
2024-10-05 19:54:58 +0300

    feat: made all fs

--- src/fs/read.js
+++ src/fs/read.js
@@ -1,5 +1,16 @@
+import { promises } from 'fs';
+import { join } from 'path';
+
 const read = async () => {
-    // Write your code here 
+    const filePath = join(process.cwd(), 'files', 'fileToRead.txt');
+
+    try {
+        await promises.access(filePath);
+        const content = await promises.readFile(filePath, 'utf8');
+        console.log(content);
+    } catch (error) {
+        throw new Error('FS operation failed');
+    }
 };
 
 await read();
--- src/fs/rename.js
+++ src/fs/rename.js
@@ -1,5 +1,34 @@
+import { promises } from 'fs';
+import { join } from 'path';
+
 const rename = async () => {
-    // Write your code here 
+    const srcFile = join(process.cwd(), 'files', 'wrongFilename.txt');
+    const destFile = join(process.cwd(), 'files', 'properFilename.md');
+
+    try {
+
+        try {
+            await promises.access(srcFile);
+        } catch (error) {
+            
+            throw new Error('FS operation failed');
+        }
+
+        try {
+            await promises.access(destFile);
+            throw new Error('FS operation failed'); 
+        } catch (error) {
+            if (error.code !== 'ENOENT') {
+                throw error; 
+            }
+        }
+
+        await promises.rename(srcFile, destFile);
+        console.log('File renamed successfully!');
+        
+    } catch (error) {
+        console.error(error.message);
+    }
 };
 
 await rename();
