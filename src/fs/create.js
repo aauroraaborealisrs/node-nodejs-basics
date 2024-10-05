644ee0b4a3ab91f9acf61dbd9e24882c55980bfb
aauroraaborealis <ludanechai@mail.ru>
2024-10-05 19:10:51 +0300

    init: start task

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,19 @@
+import { promises } from 'fs';
+import { join } from 'path';
+
 const create = async () => {
-    // Write your code here 
+    const filePath = join(process.cwd(), 'files', 'fresh.txt');
+
+    try {
+        await promises.access(filePath);
+        throw new Error('FS operation failed');
+    } catch (error) {
+        if (error.code !== 'ENOENT') {
+            throw error;
+        }
+        await promises.writeFile(filePath, 'I am fresh and young', 'utf8');
+        console.log('File created successfully!');
+    }
 };
 
 await create();
