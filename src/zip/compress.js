6e0035935fca0c10c6cc0004e52039a59a907cbf
aauroraaborealisrs <imaginepeach@mail.ru>
2024-10-07 19:46:40 +0300

    feat: add solution for zip

--- src/zip/compress.js
+++ src/zip/compress.js
@@ -1,5 +1,34 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { join } from 'path';
+import { createGzip } from 'zlib';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const compress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const inputFilePath = join(__dirname, 'files', 'fileToCompress.txt'); 
+    const outputFilePath = join(__dirname, 'files', 'archive.gz'); 
+
+    const readStream = createReadStream(inputFilePath); 
+    const writeStream = createWriteStream(outputFilePath); 
+
+    const gzip = createGzip(); 
+
+    readStream.pipe(gzip).pipe(writeStream);
+
+    writeStream.on('finish', () => {
+        console.log('File compressed successfully!');
+    });
+
+    writeStream.on('error', (err) => {
+        console.error('Error writing to file:', err.message);
+    });
+
+    readStream.on('error', (err) => {
+        console.error('Error reading the file:', err.message);
+    });
 };
 
 await compress();
--- src/zip/decompress.js
+++ src/zip/decompress.js
@@ -1,5 +1,34 @@
+import { createReadStream, createWriteStream } from 'fs';
+import { join } from 'path';
+import { createGunzip } from 'zlib';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const decompress = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const inputFilePath = join(__dirname, 'files', 'archive.gz'); 
+    const outputFilePath = join(__dirname, 'files', 'fileToCompress.txt'); 
+
+    const readStream = createReadStream(inputFilePath); 
+    const writeStream = createWriteStream(outputFilePath); 
+
+    const gunzip = createGunzip(); 
+    
+    readStream.pipe(gunzip).pipe(writeStream);
+
+    writeStream.on('finish', () => {
+        console.log('File decompressed successfully!');
+    });
+
+    writeStream.on('error', (err) => {
+        console.error('Error writing to file:', err.message);
+    });
+
+    readStream.on('error', (err) => {
+        console.error('Error reading the file:', err.message);
+    });
 };
 
 await decompress();
