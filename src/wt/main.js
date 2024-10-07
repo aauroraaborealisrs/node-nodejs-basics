be3eb1f4a80c5a1d40e4c4ca2bdd7ef5db22f5cf
aauroraaborealisrs <imaginepeach@mail.ru>
2024-10-07 20:39:11 +0300

    feat: add wt solution

--- src/wt/main.js
+++ src/wt/main.js
@@ -1,5 +1,35 @@
+import { cpus } from 'os';
+import { Worker } from 'worker_threads';
+import { fileURLToPath } from 'url';
+import { dirname, join } from 'path';
+
 const performCalculations = async () => {
-    // Write your code here
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+    const workerPath = join(__dirname, 'worker.js');
+    const cpuCount = cpus().length;
+    const results = [];
+  
+    const createWorker = (index) => {
+      return new Promise((resolve) => {
+        const worker = new Worker(workerPath, { workerData: { num: 10 + index } });
+  
+        worker.on('message', (data) => {
+          results[index] = { status: 'resolved', data };
+          resolve();
+        });
+  
+        worker.on('error', () => {
+          results[index] = { status: 'error', data: null };
+          resolve();
+        });
+      });
+    };
+  
+    const workerPromises = Array.from({ length: cpuCount }, (_, index) => createWorker(index));
+    await Promise.all(workerPromises);
+  
+    console.log(results);
 };
 
 await performCalculations();
