cc24ea53710ebae1801e88269e7a5be9a491c106
aauroraaborealisrs <imaginepeach@mail.ru>
2024-10-07 19:31:17 +0300

    feat: add solution for streams

--- src/streams/read.js
+++ src/streams/read.js
@@ -1,5 +1,17 @@
+import { createReadStream } from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const read = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url); 
+    const __dirname = dirname(__filename); 
+    const filePath = join(__dirname, 'files', 'fileToRead.txt'); 
+    const stream = createReadStream(filePath, 'utf8');
+    stream.pipe(process.stdout); 
+    stream.on('error', (err) => {
+        console.error('Error reading the file:', err.message);
+    });
 };
 
 await read();
--- src/streams/write.js
+++ src/streams/write.js
@@ -1,5 +1,19 @@
+import { createWriteStream } from 'fs';
+import { join } from 'path';
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
+
 const write = async () => {
-    // Write your code here 
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = dirname(__filename);
+
+    const filePath = join(__dirname, 'files', 'fileToWrite.txt'); 
+    const writeStream = createWriteStream(filePath, 'utf8'); 
+    process.stdin.pipe(writeStream); 
+
+    writeStream.on('error', (err) => {
+        console.error('Error writing to file:', err.message);
+    });
 };
 
 await write();
